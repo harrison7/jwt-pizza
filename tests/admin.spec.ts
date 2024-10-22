b742612b7b8e462710b7032916329ec1bd01586d
harrison7 <harrison@thecaspers.net>
2024-10-22 15:53:22 -0600

    Switch backend to personal pizza service

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -31,13 +31,57 @@ test('create franchise', async ({ page }) => {
 });
 
 test('create store', async ({ page }) => {
+  await page.route('*/**/api/order/menu', async (route) => {
+    const menuRes = [
+      { id: 1, title: 'Veggie', image: 'pizza1.png', price: 0.0038, description: 'A garden of delight' },
+      { id: 2, title: 'Pepperoni', image: 'pizza2.png', price: 0.0042, description: 'Spicy treat' },
+    ];
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: menuRes });
+  });
+
   await page.route('*/**/api/franchise', async (route) => {
-    if (route.request().method() === 'GET') {
-      const createRes = [{ id: 1, name: 'pizzaPocket', stores: [{ id: 1, name: 'SLC' }] }]
-      expect(route.request().method()).toBe('GET');
-      // expect(route.request().postDataJSON()).toMatchObject(createReq);
-      await route.fulfill({ json: createRes });
-    }
+    const franchiseRes = [
+      {
+        id: 2,
+        name: 'LotaPizza',
+        stores: [
+          { id: 4, name: 'Lehi' },
+          { id: 5, name: 'Springville' },
+          { id: 6, name: 'American Fork' },
+        ],
+      },
+      { id: 3, name: 'PizzaCorp', stores: [{ id: 7, name: 'Spanish Fork' }] },
+      { id: 4, name: 'topSpot', stores: [] },
+    ];
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: franchiseRes });
+  });
+
+  await page.route('*/**/api/order', async (route) => {
+    const orderReq = {
+      items: [
+        { menuId: 1, description: 'Veggie', price: 0.0038 },
+        { menuId: 2, description: 'Pepperoni', price: 0.0042 },
+      ],
+      storeId: '4',
+      franchiseId: 2,
+    };
+    const orderRes = {
+      order: {
+        items: [
+          { menuId: 1, description: 'Veggie', price: 0.0038 },
+          { menuId: 2, description: 'Pepperoni', price: 0.0042 },
+        ],
+        storeId: '4',
+        franchiseId: 2,
+        id: 23,
+      },
+      jwt: 'eyJpYXQ',
+    };
+    expect(route.request().method()).toBe('POST');
+    expect(route.request().postDataJSON()).toMatchObject(orderReq);
+    await route.fulfill({ json: orderRes });
   });
   await page.route('*/**/api/franchise/1/store', async (route) => {
     const createReq = { 
